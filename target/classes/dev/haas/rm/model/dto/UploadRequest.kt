5305bf5e423471abf03aa37ca062f923b6d68882
Unknown Haas <sharmasuhas450@gmail.com>
2025-05-28 21:17:11 +0530

    refresah

--- target/classes/dev/haas/rm/model/dto/UploadRequest.kt
+++
@@ -1,9 +0,0 @@
-package dev.haas.rm.model.dto
-
-import org.springframework.web.multipart.MultipartFile
-
-data class UploadRequest(
-    val resumeFile: MultipartFile,
-    val JD: String,
-    val title:String,
-)
--- target/classes/dev/haas/rm/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package dev.haas.rm.repository
-
-import dev.haas.rm.model.entity.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByEmail(emailId:String): User?
-}
