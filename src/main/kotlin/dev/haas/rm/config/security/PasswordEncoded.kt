6152ba5662697ddf043642c5619a771598329a80
Unknown Haas <sharmasuhas450@gmail.com>
2025-05-28 17:25:46 +0530

    feat: Add Encoder component for password encoding and matching

--- src/main/kotlin/dev/haas/rm/config/security/PasswordEncoded.kt
+++ src/main/kotlin/dev/haas/rm/config/security/PasswordEncoded.kt
@@ -2,7 +2,14 @@ package dev.haas.rm.config.security
 
 import com.google.firebase.auth.hash.Bcrypt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Component
 
-class Encoder{
-    private val pwdEncoder= BCryptPasswordEncoder()
+@Component
+class Encoder {
+
+    private val pwdEncoder = BCryptPasswordEncoder()
+
+    fun encode(pwd:String):String=pwdEncoder.encode(pwd)
+
+    fun isMatch(pwd:String,hashed:String): Boolean=pwdEncoder.matches(pwd,hashed)
 }
